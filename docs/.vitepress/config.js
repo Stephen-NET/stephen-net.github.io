06db9eb4d52b94df6b7d9048df4e82fa3b2c10af
LiangChen <chli_1990@163.com>
2023-03-07 22:34:24 +0800

    update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,17 @@
 export default {
     title: '知识库',
-    description: 'Just playing around.'
+    description: 'Just playing around.',
+    themeConfig: {
+        nav: [
+            { text: 'Guide', link: '/guide' },
+            {
+                text: 'Dropdown Menu',
+                items: [
+                    { text: 'Item A', link: '/item-1' },
+                    { text: 'Item B', link: '/item-2' },
+                    { text: 'Item C', link: '/item-3' }
+                ]
+            }
+        ]
+    }
 }
