c966ca03ec551c3d662044311991d4fc940eb37a
LiangChen <chli_1990@163.com>
2023-03-07 23:19:16 +0800

    update sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -13,14 +13,31 @@ export default {
                 ]
             }
         ],
-        sidebar: [
-            {
-              text: 'Guide',
-              items: [
-                { text: 'Introduction', link: '/introduction' },
-                { text: 'Getting Started', link: '/getting-started' },
-              ]
-            }
-          ]
+        sidebar: {
+            // This sidebar gets displayed when user is under `guide` directory.
+            '/guide/': [
+                {
+                    text: 'Guide',
+                    items: [
+                        // This shows `/guide/index.md` page.
+                        { text: 'Index', link: '/guide/' }, // /guide/index.md
+                        { text: 'One', link: '/guide/one' }, // /guide/one.md
+                        { text: 'Two', link: '/guide/two' } // /guide/two.md
+                    ]
+                }
+            ],
+            // This sidebar gets displayed when user is under `config` directory.
+            '/config/': [
+                {
+                    text: 'Config',
+                    items: [
+                        // This shows `/config/index.md` page.
+                        { text: 'Index', link: '/config/' }, // /config/index.md
+                        { text: 'Three', link: '/config/three' }, // /config/three.md
+                        { text: 'Four', link: '/config/four' } // /config/four.md
+                    ]
+                }
+            ]
+        }
     }
 }
